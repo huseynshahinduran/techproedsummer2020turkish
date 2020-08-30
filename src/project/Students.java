695b8f27d19331e46690767e669d0493767ba9c7
Huseyn <huseyinsahinduran@gmail.com>
2020-08-30 22:02:02 +0100

    techproed summer 2020 turkish1

--- src/project/Students.java
+++ src/project/Students.java
@@ -1,5 +1,7 @@
 package project;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Students {
@@ -50,6 +52,105 @@ public class Students {
 		}while(true);
 		
 		System.out.println("Enrolled in: "+courses);
+	}
+	
+	public void payTuition() {
+		System.out.println("Your balance: $" + tuitionBalance);
+		Scanner scan = new Scanner(System.in);
+		System.out.println("How much do you want to pay?");
+		int payment = scan.nextInt();
+		tuitionBalance = tuitionBalance - payment;
+		System.out.println("Thanks for payment of $" + payment);
+		System.out.println("Your balance: $" + tuitionBalance);
+	}
+	
+	public String toString() {
+		return  "STUDENT ID: " + studentId +
+				"\nSTUDENT NAME: " + firstName + " " + lastName +
+				"\nGRADE LEVEL: " + gradeYear +
+				"\nCOURSES ENROLLED: " + courses +
+				"\nREMAINING BALANCE:" + tuitionBalance;
+	}
+	
+	static Map<String, String> studentInfo = new HashMap<>();
+	
+	public static void addStudentsToMap() {
+		Scanner scan = new Scanner(System.in);
+		String stop = "";
+		while(!stop.equals("S")) {
+			Students s = new Students();
+			s.enrool();
+			s.payTuition();
+			studentInfo.put(s.studentId, s.toString());
+			System.out.println("Press 'S' to stop entrance");
+			System.out.println("Press Enter to go on entrance");
+			stop = scan.nextLine();
+		}
+		selectOptions();
+	}
+	
+	public static void getStudentsInfo() {
+		Scanner scan = new Scanner(System.in);
+		String idOfStudent = "";
+		String result = "";
+		
+		do {
+			System.out.println("Enter student id to get information");
+			System.out.println("Enter 'X' to end the program");
+			idOfStudent = scan.nextLine();
+			result = studentInfo.get(idOfStudent);
+			
+			if(!idOfStudent.equals("X")) {
+				System.out.println(result);
+			}
+		}while(!idOfStudent.equals("X"));
+		
+		selectOptions();
+	}
+	
+	public static void removeStudent() {
+		Scanner scan = new Scanner(System.in);
+		String idOfStudent = "";
+		String result = "";
+		
+		do {
+			System.out.println("Enter student id to remove");
+			System.out.println("Press 'X' to end the program");
+			
+			idOfStudent = scan.nextLine();
+			result = studentInfo.remove(idOfStudent);//remove() with key returns the value in a map
+			
+			if(!idOfStudent.equals("X")) {
+				System.out.println(result);
+				System.out.println("Removed from the students list");
+			}	
+		}while(!idOfStudent.equals("X"));
+		selectOptions();
+	}
+	
+	public static void selectOptions() {
+		System.out.println("Select the options:");
+		System.out.println("1: Add students");
+		System.out.println("2: Get the student's information by id");
+		System.out.println("3: Remove student  by id");
+		
+		Scanner scan = new Scanner(System.in);
+		int option = scan.nextInt();
+		
+		switch(option) {
+			case 1:
+				addStudentsToMap();
+				break;
+			case 2:
+				getStudentsInfo();
+				break;
+			case 3:
+				removeStudent();
+				break;
+			default:
+				System.out.println("Invalid selection, select one of 1, 2, or 3");
+		}
+		selectOptions();
 	}
 
 }
